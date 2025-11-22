7dff2facc7841ceada46afbda0a8bebeee57e00b
Dror Granot <dror091@gmail.com>
2025-11-22 09:01:13 +0200

    chore: Remove SQL and PowerShell scripts from BE folder (moved to MetaData/Scripts)
    - Scripts have been organized into MetaData/Scripts folder
    - No functional changes to backend code
    - Seeding service unchanged

--- create_db.sql
+++
@@ -1,3 +0,0 @@
-﻿-- Create the database if it doesn't exist
-SELECT 'CREATE DATABASE amesa_lottery' 
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'amesa_lottery')\gexec
--- filter-secrets.ps1
+++
@@ -1,9 +0,0 @@
-$file = "AmesaBackend/appsettings.Development.json"
-if (Test-Path $file) {
-    $content = Get-Content $file -Raw | ConvertFrom-Json
-    $content.Authentication.Google.ClientId = ""
-    $content.Authentication.Google.ClientSecret = ""
-    $content | ConvertTo-Json -Depth 10 | Set-Content $file -NoNewline
-    git add $file
-}
-
