d9d6e028eaa9896c3b3a7355c42929503d5d6275
Dror Granot <dror091@gmail.com>
2025-12-30 18:39:48 +0200

    Fix PostgreSQL column name mappings for LotteryTicket entity - maps UserId, HouseId, PurchasePrice, CreatedAt, UpdatedAt, TicketNumber to snake_case columns

--- BE/AmesaBackend.Lottery/Data/LotteryDbContext.cs
+++ BE/AmesaBackend.Lottery/Data/LotteryDbContext.cs
@@ -37,6 +37,12 @@ public class LotteryDbContext : DbContext
         {
             entity.ToTable("lottery_tickets", "amesa_lottery");
             entity.HasKey(e => e.Id);
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.HouseId).HasColumnName("house_id");
+            entity.Property(e => e.PurchasePrice).HasColumnName("purchase_price");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_at");
+            entity.Property(e => e.UpdatedAt).HasColumnName("updated_at");
+            entity.Property(e => e.TicketNumber).HasColumnName("ticket_number");
             entity.HasOne(e => e.House)
                 .WithMany()
                 .HasForeignKey(e => e.HouseId)
